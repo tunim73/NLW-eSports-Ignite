81729cfdf2ae7c18fd9ea7aade83f53e66668f3b
tunim73 <antonio.netto973@gmail.com>
2023-01-24 23:40:16 -0300

    feature mobile: Home part 1

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,22 @@
+import { Image, View } from 'react-native';
+
+import { styles } from './styles';
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../../components/Heading';
+
+
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      />
+      <Heading
+      title='Encontre seu duo!'
+      subtitle='Selecione o game que deseja jogar...' 
+      />
+    </View>
+  );
+}
